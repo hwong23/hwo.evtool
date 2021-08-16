b621e5bdc29f7bc4aa849f34ef6d0b73653071d3
Harry Wong <hwong23@gmail.com>
2021-08-15 19:37:55 -0500

    certificacion --1

---
+++ criterio/src/hwo/evtool/equipo/educacion/CriterioCertificacion.java
@@ -0,0 +1,25 @@
+package hwo.evtool.equipo.educacion;
+
+import hwo.evtool.evaluacion.CriterioEvaluacion;
+import hwo.evtool.evaluacion.Evaluacion;
+
+class CriterioCertificacion extends CriterioEvaluacion {
+	private Evaluacion evaluar;
+	private String nombre = "Certificacion";
+
+	public CriterioCertificacion(Evaluacion evaluar) {
+		this.evaluar = evaluar;
+	}
+
+	public String getDescription() {
+		return evaluar.getDescription() + ", " + this.nombre;
+	}
+
+	public double cost() {
+		String experiencia = Evaluacion.edata.get("experiencia");
+		double costo = Double.parseDouble(experiencia) * 2;
+		
+		System.out.println("--- " + this.nombre + " " + costo);
+		return costo + evaluar.cost();
+	}
+}
